52f9334a289b4f1291f923cdfe2883514b187d81
Mikke Schirén <mikke.schiren@digitalistgroup.com>
2021-11-18 17:23:00 +0100

    first things

---
+++ TrackerDomain.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\TrackerDomain;
+
+use Piwik\Config;
+
+class TrackerDomain extends \Piwik\Plugin
+{
+
+    public function registerEvents()
+    {
+        return [
+            'Tracker.getJavascriptCode' => 'setPiwikUrl',
+        ];
+    }
+
+    /**
+     * Set the URL from config,
+     */
+
+    public function setPiwikUrl(&$codeImpl, $parameters)
+    {
+
+            $config = Config::getInstance()->TrackerDomain;
+            $url = $config['url'];
+        if (isset($url)) {
+            $codeImpl["piwikUrl"] = $url;
+        }
+    }
+}
